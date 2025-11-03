0d32230fdde53d3ce36fcb40cb0e65f07c0eca86
taegeunsong <tgsong@thingspire.com>
2025-11-03 10:55:31 +0900

    Initial commit

---
+++ src/main/java/com/spring_problem_solver/SpringProblemSolverApplication.java
@@ -0,0 +1,13 @@
+package com.spring_problem_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProblemSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProblemSolverApplication.class, args);
+	}
+
+}
